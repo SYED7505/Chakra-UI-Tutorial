385bbb448a091b0076a694c19b8e11e47d370241
SYED7505 <syed_zabiulla@thbs.com>
2024-05-28 15:51:10 +0530

    Chakra Form Fields + React-router Forms

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,60 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+  Textarea,
+  HStack,
+  Checkbox,
+  Icon,
+  Button,
+} from "@chakra-ui/react";
+import { Form, redirect } from "react-router-dom";
+
 export default function Create() {
   return (
-    <div>Create</div>
-  )
+    <Box maxW="490px">
+      <Form method="POST" action="/create">
+        <FormControl isRequired mb="40px">
+          <FormLabel>Task:</FormLabel>
+          <Input type="text" name="title" />
+        </FormControl>
+
+        <FormControl mb="40px">
+          <FormLabel>Description</FormLabel>
+          <Textarea
+            placeholder="Enter detailed description of the task.."
+            name="description"
+          />
+        </FormControl>
+
+        <FormControl>
+          <HStack alignItems="center">
+            <Box>
+              <Icon as={Checkbox} colorScheme="orange" name="priority" />
+            </Box>
+            <FormLabel ml="10px">Make this priority task</FormLabel>
+          </HStack>
+        </FormControl>
+
+        <Button mt="20px" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Box>
+  );
 }
+
+export const createAction = async ({ request }) => {
+  const data = await request.formData();
+
+  const task = {
+    title: data.get("title"),
+    description: data.get("description"),
+    isPriority: data.get("priority") === "",
+  };
+
+  console.log(task);
+
+  return redirect("/");
+};
